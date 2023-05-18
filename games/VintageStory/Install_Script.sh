ba2ee75641f5562e729cf36cc0d6a293e796b7c1
Nick <133668892+NickHostingClub@users.noreply.github.com>
2023-05-18 02:09:13 +0200

    Update Install_Script.sh
    Translate from Dutch to English

--- games/VintageStory/Install_Script.sh
+++ games/VintageStory/Install_Script.sh
@@ -2,10 +2,10 @@
 set -x
 cd /mnt/server
 
-# Download de CA-bundel van Mozilla
+# Download the CA bundle from Mozilla.
 curl -O https://curl.se/ca/cacert.pem
 
-# Kies de juiste URL op basis van de release branch
+# Select the appropriate URL based on the release branch.
 if [ "${RELEASE_BRANCH}" = "stable" ]; then
     URL="https://api.vintagestory.at/stable.json"
 elif [ ${RELEASE_BRANCH} = "unstable" ]; then
@@ -13,36 +13,36 @@ elif [ ${RELEASE_BRANCH} = "unstable" ]; then
 elif [ ${RELEASE_BRANCH} = "pre" ]; then
     URL="https://api.vintagestory.at/pre.json"
 else
-    echo -e "Ongeldige release branch"
+    echo -e "Invalid release branch."
     exit 1
 fi
 
-# Haal JSON data op
+# Retrieve JSON data.
 JSON_DATA=$(curl -sS --cacert cacert.pem "$URL")
 
-# Valideer JSON data
+# Validate JSON data.
 echo -e $JSON_DATA | jq empty > /dev/null 2>&1
 if [ $? -ne 0 ]; then
-    echo -e "Ongeldige JSON data ontvangen"
+    echo -e "Received invalid JSON data"
     exit 1
 fi
 
-# Als de release version "latest" is, krijg de laatste versie nummer
+# If the release version is set to "latest," retrieve the latest version number.
 if [ "${RELEASE_VERSION}" = "latest" ]; then
     LATEST_KEY=$(echo -e "$JSON_DATA" | jq -r 'to_entries[] | select(any(.value[]; .latest == 1)) | .key')
     if [ -z "$LATEST_KEY" ]; then
-        echo -e "Laatste versie niet gevonden"
+        echo -e "Latest version not found"
         exit 1
     else
         RELEASE_VERSION=$LATEST_KEY
     fi
 fi
 
-# Zoek de CDN en de lokale URL van de te downloaden bestanden
+# Find the CDN and local URLs of the files to be downloaded.
 CDN_URL=$(echo -e "$JSON_DATA" | jq -r --arg VERSION "$RELEASE_VERSION" '.[$VERSION].server.urls.cdn')
 LOCAL_URL=$(echo -e "$JSON_DATA" | jq -r --arg VERSION "$RELEASE_VERSION" '.[$VERSION].server.urls.local')
 
-# Haal de MD5-hash op
+# Retrieve the MD5 hash.
 FILE_MD5=$(echo -e "$JSON_DATA" | jq -r --arg VERSION "$RELEASE_VERSION" '.[$VERSION].server.md5')
 
 check_http_status() {
@@ -51,47 +51,44 @@ check_http_status() {
     echo -e "$http_code"
 }
 
-# Controleer of het CDN bestand bestaat
+# Verify if the CDN file exists.
 HTTP_CODE=$(check_http_status "$CDN_URL")
 
-# Als het CDN bestand niet bestaat, controleer het lokale bestand
+# If the CDN file does not exist, check the local file.
 if [ "$HTTP_CODE" != "200" ]; then
     HTTP_CODE=$(check_http_status "$LOCAL_URL")
     if [ "$HTTP_CODE" = "200" ]; then
         FILE_URL=$LOCAL_URL
     else
-        echo -e "Bestand niet gevonden"
+        echo -e "File not found"
         exit 1
     fi
 else
     FILE_URL=$CDN_URL
 fi
 
-# Controleer of data map bestaat, zo niet, maak het aan
 if [ ! -d "data" ]; then
   mkdir data
 fi
 
-# Controleer of serverconfig.json bestaat in de data map, zo niet, download het
+# Check if serverconfig.json exists in the data directory, if not, download it.
 if [ ! -f "data/serverconfig.json" ]; then
   curl -sS --cacert cacert.pem -o data/serverconfig.json "https://raw.githubusercontent.com/NickHostingClub/eggs/main/games/VintageStory/serverconfig.json"
 fi
 
-# Download het bestand
 curl -sS --cacert cacert.pem "$FILE_URL" -o "vs_server.tar.gz"
 
-# Controleer de MD5-hash van het gedownloade bestand
+# Verify the MD5 hash of the downloaded file.
 DOWNLOADED_MD5=$(md5sum "vs_server.tar.gz" | awk '{print $1}')
 
 if [ "$DOWNLOADED_MD5" != "$FILE_MD5" ]; then
-    echo -e "MD5-hash komt niet overeen, bestand is mogelijk beschadigd of gecompromitteerd"
+    echo -e "MD5 hash does not match, the file may be corrupted or compromised"
     rm "vs_server.tar.gz"
     exit 1
 fi
 
-# Pak het bestand uit
 tar xz -f "vs_server.tar.gz"
 
-# Verwijder het gedownloade bestand
+# Remove the files
 rm "vs_server.tar.gz"
 rm "cacert.pem"
